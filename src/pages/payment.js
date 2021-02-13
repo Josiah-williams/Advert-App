cf7331bf3d89fa3502854d80175a23221a75803a
josiah <josiahdamiwilliams@gmail.com>
2021-02-13 22:02:06 +0100

    Added a payment component

--- src/pages/payment.js
+++ src/pages/payment.js
@@ -8,26 +8,27 @@ export default function payment() {
       <h1>Buy Now</h1>
       <div className="dives">
         <div className="signup-card">
-          <Link to="/appform">
+        <a target="_blank" href="https://paystack.com/buy/unlimited-weekend-vbahtd">
             <div className="img-wrapper">
               <img src={money} alt="money" className="sign-up" />
             </div>
             <button className="button-primary button-big">
               weekend Unlimited
             </button>
-          </Link>
-          <p>Buy unlimited weekend plan</p>
+          </a>
+          <p>Spend just 100 naira for the weekend for unlimited ads clicks</p>
         </div>
         <div className="signup-card">
-          <Link to="/promotion">
+        <a target="_blank" href="https://paystack.com/buy/500-clicks-mcyaew">
             <div className="img-wrapper">
               <img src ={money} alt="money" className="sign-up" />
             </div>
             <button className="button-primary button-big">
               500 clicks
             </button>
-          </Link>
-          <p>Buy 500 clicks</p>
+          </a>
+          <p>Increase sales with greenhood ads for just 20kobo per click, 500 Clicks for just #100
+          </p>
         </div>
       </div>
     </div>
