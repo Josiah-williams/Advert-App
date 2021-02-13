70a889e114c46caf53134134293d653e5f3876a2
josiah <josiahdamiwilliams@gmail.com>
2021-02-13 22:02:44 +0100

    removed unuseful link from images folder

--- src/components/images.js
+++ src/components/images.js
@@ -1,7 +1,7 @@
 export default [
     {
       src: "https://drive.google.com/uc?id=1918NohcdkP7__l6wHmaaEtSRTLBB3wJb",
-      link:"/Register"
+      
     },
     {
       src: "https://drive.google.com/uc?id=15kR4GApJ0OzzxK4iJcf16-Q7cMLuW-6z",
