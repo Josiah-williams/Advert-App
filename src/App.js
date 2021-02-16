0a5294c56f61c683e69f52835f304d4868bd319d
josiah <josiahdamiwilliams@gmail.com>
2021-02-16 20:20:58 +0100

    made the logo dormant throughout the whole app

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import {  logout, } from "./state/actionCreators";
 import Login from "./components/user/Login"
 import Register from "./components/user/Register"
 import "./CSS/App.css"
-import Navbar from "./components/user/Navbar"
+import styled from "styled-components"
 import Promotion from "./components/AdvertForm"
 import Images from './components/images'
 import Slider from './components/Slider'
 import AppForm from './components/AppForm'
+import UserDashboard from './components/UserDashboard'
+import {
+  tabletPortrait,
+  tabletLandscape,
+  mobilePortrait,
+  mobileLandscape,
+  FlexFunc,
+  tabletPortraitLarge,} 
+from "./styles/theme.styles"
 // import privateRoute from "../../utils/privateRoute";
 // import './App.css'
 import Payment from "./pages/payment";
+import AppinfoContainer from "./components/AppinfoContainer"
+import logo from "./icons/123924428_705557600377412_3955865959784530213_n.jpg";
 
 function App({ appState, user, checkToken, logout }) {
   
@@ -21,9 +32,14 @@ function App({ appState, user, checkToken, logout }) {
   }, []);
   return (
     <div className="App">
-    <Navbar appState={appState} user={user} logout={logout} />
+   
 
     <div className="container">
+      <Logo>
+    <div className="logo-div">
+          <img src={logo} className="logo" />
+          </div>
+          </Logo>
     <Switch>
     <Route exact path="/">
     <Slider images={Images}/>
@@ -43,6 +59,13 @@ function App({ appState, user, checkToken, logout }) {
             <Route exact path="/payment">
             <Payment />
             </Route>
+            <Route exact path="/info">
+              <AppinfoContainer />
+              </Route>
+              <Route exact path="/Dashboard">
+              <UserDashboard />
+            </Route>
+
 
           </Switch>
           </div>
@@ -56,4 +79,23 @@ function mapStateToProps(state) {
     user: state.user
   };
 }
+const Logo = styled.div`
+.logo {
+  z-index: 1;
+  padding-top: 10px;
+  position: absolute;
+  top: -9px;
+  left:1px;
+  @media ${mobilePortrait} {
+    z-index: 1;
+    padding-top: 33px;
+    height: 87px;
+    width: 79px;
+    .img {
+      position: sticky;
+      top: 0;
+    }
+  }
+}
+`;
 export default connect(mapStateToProps, { logout })(App);
